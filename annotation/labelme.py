dd77d8eea65ff5eff0c90c62390a639fa396cbfb
DamonLiu <damon.liu@alpha-info.com.tw>
2020-06-10 16:27:21 +0800

    ADD property LabelmeJSON.iaa /imgw /imgh

--- annotation/labelme.py
+++ annotation/labelme.py
@@ -59,12 +59,22 @@ class LabelmeJSON(AppBase):
         imgfolder = os.path.dirname(self.filepath)
         return os.path.join(imgfolder, imgname)
     
+    @property
+    def imgw(self):
+        self.__parse()
+        return self.data.get('imageWidth')
+    
+    @property
+    def imgh(self):
+        self.__parse()
+        return self.data.get('imageHeight')
+    
     def __get_imgfile(self):
         self.__imgfile = ImageFile(self.imgpath)
     
     @property
     def imgfile(self):
-        if 'imgfile' not in self.__dict__: self.__get_imgfile()
+        if f'_{self.__class__.__name__}__imgfile' not in self.__dict__: self.__get_imgfile()
         return self.__imgfile 
     
     def from_(self, img_path, shapes=None, flags=None):
@@ -108,3 +118,23 @@ class LabelmeJSON(AppBase):
         xml.from_(img_path=self.imgpath, shapes=rects)
         
         return xml
+    
+    @property
+    def iaa(self):
+        from imgaug.augmentables.kps import KeypointsOnImage
+        from imgaug.augmentables.bbs import BoundingBoxesOnImage
+        from imgaug.augmentables.polys import PolygonsOnImage
+        
+        pts = [pt.iaa for pt in self.shape_dict['point']]
+        boxes = [box.iaa for box in self.shape_dict['rectangle']]
+        polys = [poly.iaa for poly in self.shape_dict['polygon']]
+        
+        imgshape = (self.imgh, self.imgw)
+        shapes_on_image = {
+            'keypoints': KeypointsOnImage(pts, imgshape),
+            'bounding_boxes': BoundingBoxesOnImage(boxes, imgshape),
+            'polygons': PolygonsOnImage(polys, imgshape),
+        }
+        
+        return shapes_on_image
+    
